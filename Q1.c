78e850b12d39b8eb02fe2d9e2661ca73c2d94ae4
Junior Teixeira <juniorteixeira1805@ufrn.edu.br>
2021-02-13 16:04:32 -0300

    correção

--- Q1.c
+++ Q1.c
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
 
     //Determine o valor das seguintes expressões:
 
-    p == &i; // true ou 1: p é um ponteiro para o endereço de da variavel i e &i é o indereço de i, logo p == &i é verdadeiro
+    p == &i; // true ou 1: p é um ponteiro para o endereço de da variavel i e &i é o endereço de i, logo p == &i é verdadeiro
     *p - *q; // -2: valor de j - valor de i: 5-3=-2 
     **&p; // 3, o valor de i - desreferênciação do ponteiro para inteiro p
     3 - *p/(*q) + 7; // 10 - (*p/*q) = 0, pois o resultado inteiro de 3/5 = 0,  então sobra 3 + 7 = 10
